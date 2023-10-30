6b3dc31dbde98c058f4e11d02b5d6dc6493c44b2
kafeijao <kafeijao@gmail.com>
2023-10-30 18:37:51 +0000

    Added CVR Pointer particle system support.

---
+++ ml_prm/RagdollTriggerVolume.cs
@@ -0,0 +1,14 @@
+﻿using ABI_RC.Core.Savior;
+using ABI.CCK.Components;
+using UnityEngine;
+
+namespace ml_prm {
+
+    public class RagdollTriggerVolume : CVRTriggerVolume
+    {
+        public Collider collider { get; set; }
+        public RagdollTrigger trigger { get; set; }
+        public void TriggerEnter(CVRPointer pointer) => trigger.OnPointerParticleEnter(pointer);
+        public void TriggerExit(CVRPointer pointer)  => trigger.OnPointerParticleExit(pointer);
+    }
+}
