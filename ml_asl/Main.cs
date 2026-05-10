aa8def319292d4b7d0c6cb2550b57f52b1019124
SDraw <sdraw@protonmail.com>
2026-05-10 11:53:09 +0300

    Forgotten eye blinking

--- ml_asl/Main.cs
+++ ml_asl/Main.cs
@@ -1,4 +1,5 @@
 using ABI_RC.Core.Player;
+using ABI_RC.Core.Savior;
 using ABI_RC.Systems.FaceTracking;
 using System;
 using System.Reflection;
@@ -8,8 +9,6 @@ namespace ml_asl
 {
     public class AvatarSyncedLook : MelonLoader.MelonMod
     {
-        readonly static Matrix4x4 ms_back = Matrix4x4.Translate(Vector3.back);
-
         public override void OnInitializeMelon()
         {
             HarmonyInstance.Patch(
@@ -28,14 +27,24 @@ namespace ml_asl
         {
             try
             {
-                if(Settings.Enabled && (__instance.EyeMovementController != null) && !FaceTrackingManager.Instance.IsEyeDataAvailable())
+                if(Settings.Enabled && (__instance.EyeMovementController != null))
                 {
-                    ____playerAvatarMovementData.EyeTrackingOverride = true;
+                    if(!FaceTrackingManager.Instance.IsEyeDataAvailable())
+                    {
+                        ____playerAvatarMovementData.EyeTrackingOverride = true;
 
-                    if(__instance.EyeMovementController.CurrentTarget != null)
-                        ____playerAvatarMovementData.EyeTrackingPosition = __instance.EyeMovementController.CurrentTarget.GetPosition();
-                    else
-                        ____playerAvatarMovementData.EyeTrackingPosition = (__instance.transform.GetMatrix() * ms_back).GetPosition();
+                        if(__instance.EyeMovementController.CurrentTarget != null)
+                            ____playerAvatarMovementData.EyeTrackingPosition = __instance.EyeMovementController.CurrentTarget.GetPosition();
+                        else
+                        {
+                            Transform l_camera = (!MetaPort.Instance.isUsingVr ? __instance.desktopCamera : __instance.vrCamera).transform;
+                            ____playerAvatarMovementData.EyeTrackingPosition = l_camera.position - l_camera.forward;
+                        }
+
+                        ____playerAvatarMovementData.EyeBlinkingOverride = true;
+                        ____playerAvatarMovementData.EyeTrackingBlinkProgressLeft = __instance.EyeMovementController.blinkProgressLeft;
+                        ____playerAvatarMovementData.EyeTrackingBlinkProgressRight = __instance.EyeMovementController.blinkProgressRight;
+                    }
                 }
             }
             catch(Exception e)
--- ml_asl/Utils.cs
+++ ml_asl/Utils.cs
@@ -9,11 +9,5 @@ namespace ml_asl
         static readonly FieldInfo ms_view = typeof(CohtmlControlledViewWrapper).GetField("_view", BindingFlags.Instance | BindingFlags.NonPublic);
 
         public static void ExecuteScript(this CohtmlControlledViewWrapper p_instance, string p_script) => (ms_view?.GetValue(p_instance) as cohtml.Net.View)?.ExecuteScript(p_script);
-
-        // Extensions
-        public static Matrix4x4 GetMatrix(this Transform p_transform, bool p_pos = true, bool p_rot = true, bool p_scl = false)
-        {
-            return Matrix4x4.TRS(p_pos ? p_transform.position : Vector3.zero, p_rot ? p_transform.rotation : Quaternion.identity, p_scl ? p_transform.localScale : Vector3.one);
-        }
     }
 }
